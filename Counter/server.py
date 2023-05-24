68fc70409c106892c3c5474354eafdbcb3a6583a
CWLunas <120340798+CWLunas@users.noreply.github.com>
2023-05-23 16:16:54 -0800

    Add files via upload

---
+++ Counter/server.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, session, redirect
+
+app = Flask(__name__)
+
+app.secret_key="Go Ahead, Make My Day"
+
+@app.route('/')
+def index():
+    if "count" not in session:
+        session["count"] = 0
+    else:
+        session['count'] += 1
+    return render_template("index.html")
+
+@app.route('/double')
+def inde2():
+    if "count" not in session:
+        session["count"] = 0
+    else:
+        session['count'] += 2
+    return render_template("index.html")
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
+
+if __name__=="__main__":
+    app.run(debug=True)
